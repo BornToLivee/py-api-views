377bcca3c21b9baec4e8cf3b72c0989fad58b125
Bohdan Zinchenko <bogdan.zinchenko.2019@gmail.com>
2024-07-24 22:31:09 +0300

    py-api-views v1

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,10 +1,73 @@
 from django.urls import path
 
-from cinema.views import movie_list, movie_detail
+from cinema import views
+
+movie_list = views.MovieViewSet.as_view(actions={
+    "get": "list",
+    "post": "create"
+})
+
+movie_detail = views.MovieViewSet.as_view(actions={
+    "get": "retrieve",
+    "put": "update",
+    "patch": "partial_update",
+    "delete": "destroy"
+})
 
 urlpatterns = [
-    path("movies/", movie_list, name="movie-list"),
-    path("movies/<int:pk>/", movie_detail, name="movie-detail"),
+    path(
+        "genres/",
+        views.GenreList.as_view(),
+        name="genre-list"
+    ),
+    path(
+        "genres/<int:pk>/",
+        views.GenreDetail.as_view(),
+        name="genre-detail"
+    ),
+    path(
+        "actors/",
+        views.ActorList.as_view(),
+        name="actor-list"
+    ),
+    path(
+        "actors/<int:pk>/",
+        views.ActorDetail.as_view(),
+        name="actor-detail"
+    ),
+    path(
+        "cinema_halls/",
+        views.CinemaHallViewSet.as_view(
+            actions=
+            {
+                "get": "list",
+                "post": "create"
+            }
+        ),
+        name="cinema-hall-list"
+    ),
+    path(
+        "cinema_halls/<int:pk>/",
+        views.CinemaHallViewSet.as_view(
+            actions={
+                "get": "retrieve",
+                "put": "update",
+                "patch": "partial_update",
+                "delete": "destroy"
+            }
+        ),
+        name="cinema-hall-detail"
+    ),
+    path(
+        "movies/",
+        movie_list,
+        name="movie-list"
+    ),
+    path(
+        "movies/<int:pk>/",
+        movie_detail,
+        name="movie-detail"
+    ),
 ]
 
 app_name = "cinema"
